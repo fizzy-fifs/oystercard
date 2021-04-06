710f74e172d9c32e2399aa14c1451b915d06e4ab
Fifolu Akinyemi <fkinyemi@gmail.com>
2021-04-06 16:02:27 +0100

    Added balance method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,7 @@
-
+require 'oystercard'
 describe Oystercard do
-  
+  oystercard = Oystercard.new
+  it { expect(oystercard).to be_an_instance_of(Oystercard) }
+  it { expect(subject).to respond_to(:balance) }
+  it { expect(subject.balance).to eq (0) }
 end
