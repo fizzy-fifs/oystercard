7b037676109e5f56236914b81fa9020a8ddc935f
Fifolu Akinyemi <fkinyemi@gmail.com>
2021-04-06 17:19:09 +0100

    Added a function that raises an error if the top-up value will take the balance over the maximum value: £90

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,12 @@
 class Oystercard
+  MaxBalance = 90
   attr_reader :balance
   def initialize(balance = 0)
     @balance = balance
   end
 
   def top_up(amount)
+    raise "Exceeded maximum amount: £#{MaxBalance}" if @balance + amount > MaxBalance
     @balance += amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,4 +11,10 @@ describe Oystercard do
     oystercard.top_up(15)
     expect { subject.top_up(15) }.to change { subject.balance }.by(15)
   end
+
+  it "throws an error when balance exceed 90" do
+    oystercard = Oystercard.new
+    new_balance = oystercard.top_up(9)
+    expect { new_balance }.to raise_error "Exceeded maximum amount of: £#{MaxBalance}" if new_balance > 90
+  end
 end
