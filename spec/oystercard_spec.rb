9a541c62dacbe947fc5257fcd904161a2db523b7
Fifolu Akinyemi <fkinyemi@gmail.com>
2021-04-06 16:37:59 +0100

    Challenge 5 done

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,11 @@ describe Oystercard do
   it { expect(oystercard).to be_an_instance_of(Oystercard) }
   it { expect(subject).to respond_to(:balance) }
   it { expect(subject.balance).to eq (0) }
+  it { expect(subject).to respond_to(:top_up).with(1).argument }
+  
+  it "changes the balance value" do
+    oystercard = Oystercard.new
+    oystercard.top_up(15)
+    expect { subject.top_up(15) }.to change { subject.balance }.by(15)
+  end
 end
